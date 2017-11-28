79dcf62ec3d331f26cf3a6dcda98098b605dba29
jialuo.wang <jialuo.wang@mullenmail.com>
2017-11-28 14:57:46 -0700

    new

---
+++ Group Selector jeffrey/src/sortclass.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+
+public class sortclass
+	{
+		public static void main(String[] args)
+			{
+				
+			}
+		public sortclass()
+			{
+				Collections.sort(Student, new ScoreSorter());
+			}
+	}
